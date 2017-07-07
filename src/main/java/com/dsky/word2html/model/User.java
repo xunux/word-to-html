ce4b3c882eb8a2178e14d73f67ae28578297e8b4
loi.siau <loisiau@gmail.com>
2017-07-07 11:24:03 +0800

    用jackson序列化反序列化json

--- src/main/java/com/dsky/word2html/model/User.java
+++ src/main/java/com/dsky/word2html/model/User.java
@@ -2,9 +2,12 @@ package com.dsky.word2html.model;
 
 import java.util.Date;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * spring mvc 中传 json日期的转换使用 jsonFormat
@@ -12,15 +15,19 @@ import com.fasterxml.jackson.annotation.JsonFormat;
  * 参考：
  * 	http://blog.csdn.net/zhanngle/article/details/24123659
  * 
+ * JsonIgnoreProperties 参考 
+ * 	https://github.com/FasterXML/jackson-databind/issues/95
+ * 
  * @author lois.xiao
  *
  */
+
+@JsonIgnoreProperties(value={"birth"}, allowSetters=true)
 public class User {
 	private String userId;
 	private String name;
 	
 //	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	private Date birth;
 	
 	public String getUserId() {
@@ -35,9 +42,19 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Date getBirth() {
+	
+	@JsonProperty(value="birth_day")
+	@JsonFormat(with={JsonFormat.Feature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS})
+	public Date getBirthDay() {
 		return birth;
 	}
+	
+	@JsonGetter("birth")
+	public Date getBirthDay2() {
+		return birth;
+	}
+	
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	public void setBirth(Date birth) {
 		this.birth = birth;
 	}
